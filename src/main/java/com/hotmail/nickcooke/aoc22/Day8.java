2d5ee1512595d3b517423173cf868b00d53296ef
ncooke <ncooke@heywood.co.uk>
2022-12-09 17:59:26 +0000

    Day 8 and Day 9

---
+++ src/main/java/com/hotmail/nickcooke/aoc22/Day8.java
@@ -0,0 +1,146 @@
+package com.hotmail.nickcooke.aoc22;
+
+import java.util.List;
+
+public class Day8 extends AoCSolution {
+
+    public static void main(String[] args) {
+        Day8 day8 = new Day8();
+        day8.getInput();
+        day8.part1();
+        day8.part2();
+    }
+
+    @Override
+    void part1() {
+        int[][] treeGrid = parseGrid(inputLines);
+        int visibleTrees = calculateVisibleTrees(treeGrid);
+        System.out.println("Part 1: " + (visibleTrees));
+        assert visibleTrees == 1807;
+    }
+
+
+    @Override
+    void part2() {
+        int[][] treeGrid = parseGrid(inputLines);
+        int mostViewableTrees = calculateMostViewableTrees(treeGrid);
+        System.out.println("Part 2: " + (mostViewableTrees));
+        assert mostViewableTrees == 480000;
+    }
+
+    private int calculateMostViewableTrees(int[][] treeGrid) {
+        int mostViewableTrees = 0;
+        for (int i = 0; i < treeGrid.length; i++) {
+            for (int j = 0; j < treeGrid[0].length; j++) {
+                mostViewableTrees = Math.max(mostViewableTrees, countViewableTrees(treeGrid, i, j));
+            }
+        }
+        return mostViewableTrees;
+    }
+
+    private int countViewableTrees(int[][] treeGrid, int x, int y) {
+        int left = 0;
+        int right = 0;
+        int up = 0;
+        int down = 0;
+        int treeHeight = treeGrid[x][y];
+        int heightToCompare;
+        for (int i = x + 1; i < treeGrid.length; i++) {
+            heightToCompare = treeGrid[i][y];
+            down++;
+            if (heightToCompare >= treeHeight) break;
+        }
+        for (int i = x - 1; i >= 0; i--) {
+            heightToCompare = treeGrid[i][y];
+            up++;
+            if (heightToCompare >= treeHeight) break;
+        }
+        for (int j = y + 1; j < treeGrid.length; j++) {
+            heightToCompare = treeGrid[x][j];
+            right++;
+            if (heightToCompare >= treeHeight) break;
+        }
+        for (int j = y - 1; j >= 0; j--) {
+            heightToCompare = treeGrid[x][j];
+            left++;
+            if (heightToCompare >= treeHeight) break;
+        }
+        return up * down * left * right;
+    }
+
+    private int calculateVisibleTrees(int[][] treeGrid) {
+        int count = 0;
+        for (int i = 0; i < treeGrid.length; i++) {
+            for (int j = 0; j < treeGrid[0].length; j++) {
+                count += isTreeVisible(treeGrid, i, j) ? 1 : 0;
+            }
+        }
+        return count;
+    }
+
+    private boolean isTreeVisible(int[][] treeGrid, int x, int y) {
+        boolean visible = true;
+        int treeHeight = treeGrid[x][y];
+        for (int i = x + 1; i < treeGrid.length; i++) {
+            int heightToCompare = treeGrid[i][y];
+            if (heightToCompare >= treeHeight) {
+                visible = false;
+                break;
+            }
+        }
+        if (visible) {
+            return true;
+        }
+        visible = true;
+        for (int i = x - 1; i >= 0; i--) {
+            int heightToCompare = treeGrid[i][y];
+            if (heightToCompare >= treeHeight) {
+                visible = false;
+                break;
+            }
+        }
+        if (visible) {
+            return true;
+        }
+        visible = true;
+        for (int j = y + 1; j < treeGrid.length; j
+                ++) {
+            int heightToCompare = treeGrid[x][j];
+            if (heightToCompare >= treeHeight) {
+                visible = false;
+                break;
+            }
+        }
+        if (visible) {
+            return true;
+        }
+        visible = true;
+        for (int j = y - 1; j >= 0; j--) {
+            int heightToCompare = treeGrid[x][j];
+            if (heightToCompare >= treeHeight) {
+                visible = false;
+                break;
+            }
+        }
+        return visible;
+    }
+
+    private void printGrid(int[][] treeGrid) {
+        for (int i = 0; i < treeGrid.length; i++) {
+            for (int j = 0; j < treeGrid[0].length; j++) {
+                System.out.print(treeGrid[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    private int[][] parseGrid(List<String> inputLines) {
+        int[][] treeGrid = new int[inputLines.size()][inputLines.get(0).length()];
+        for (int i = 0; i < inputLines.size(); i++) {
+            for (int j = 0; j < inputLines.get(0).length(); j++) {
+                treeGrid[i][j] = Character.getNumericValue(inputLines.get(i).charAt(j));
+            }
+        }
+        return treeGrid;
+    }
+}
---
+++ src/main/java/com/hotmail/nickcooke/aoc22/Day9.java
@@ -0,0 +1,224 @@
+package com.hotmail.nickcooke.aoc22;
+
+import java.util.*;
+
+public class Day9 extends AoCSolution {
+
+    public static void main(String[] args) {
+        Day9 day9 = new Day9();
+        day9.getInput();
+        day9.part1();
+        day9.part2();
+    }
+
+    @Override
+    void part1() {
+        int tailPositionCount = calculateTailPositions(2);
+        System.out.println("Part 1: " + tailPositionCount);
+        assert tailPositionCount == 6026;
+    }
+
+    @Override
+    void part2() {
+        int tailPositionCount = calculateTailPositions(10);
+        System.out.println("Part 2: " + tailPositionCount);
+        assert tailPositionCount == 2273;
+    }
+
+    private int calculateTailPositions(int numberOfKnots) {
+        List<Knot> rope = createRope(numberOfKnots);
+        Knot head = rope.get(0);
+        Knot tail = rope.get(rope.size() - 1);
+        for (String line : inputLines) {
+            processMove(line, head);
+        }
+        return tail.visitedPositions.size();
+    }
+
+    private List<Knot> createRope(int numberOfKnots) {
+        List<Knot> rope = new ArrayList<>();
+        for (int i = 0; i < numberOfKnots; i++) {
+            if (rope.isEmpty()) {
+                rope.add(new Knot());
+            } else {
+                rope.add(new Knot(rope.get(rope.size() - 1)));
+            }
+        }
+        return rope;
+    }
+
+    private void processMove(String line, Knot head) {
+        String move = line.split(" ")[0];
+        int steps = Integer.parseInt(line.split(" ")[1]);
+        for (int stepCount = 0; stepCount < steps; stepCount++) {
+            switch (move) {
+                case "U":
+                    head.moveUp();
+                    break;
+                case "R":
+                    head.moveRight();
+                    break;
+                case "L":
+                    head.moveLeft();
+                    break;
+                case "D":
+                    head.moveDown();
+                    break;
+                default:
+                    throw new RuntimeException("Unexpected Move");
+            }
+        }
+    }
+
+
+    class Position {
+        int x;
+        int y;
+
+        public Position(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public boolean isAdjacent(Position other) {
+            return this.x <= other.x + 1 && this.x >= other.x - 1 && this.y <= other.y + 1 && this.y >= other.y - 1 && !this.equals(other);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Position position = (Position) o;
+            return x == position.x && y == position.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+    }
+
+    class Knot {
+        private Position position;
+        private Knot previousKnot;
+        private Knot followingKnot;
+
+        Set<Position> visitedPositions = new HashSet<>();
+
+        public Knot() {
+            this(new Position(0, 0));
+        }
+
+        public Knot(Position position) {
+            this.position = position;
+            visitedPositions.add(position);
+        }
+
+        public Knot(Knot previous) {
+            this();
+            this.previousKnot = previous;
+            previous.followingKnot = this;
+        }
+
+        public boolean shouldMove() {
+            return !(position.isAdjacent(previousKnot.position) || position.equals(previousKnot.getPosition()));
+        }
+
+        public void moveTowards(Knot otherKnot) {
+            if (position.x == otherKnot.getPosition().x + 2 && position.y == otherKnot.getPosition().y) {
+                moveLeft();
+            } else if (position.x == otherKnot.getPosition().x + 2 && position.y < otherKnot.getPosition().y) {
+                moveUpAndLeft();
+            } else if (position.x == otherKnot.getPosition().x + 2 && position.y > otherKnot.getPosition().y) {
+                moveDownAndLeft();
+            } else if (position.x == otherKnot.getPosition().x - 2 && position.y == otherKnot.getPosition().y) {
+                moveRight();
+            } else if (position.x == otherKnot.getPosition().x - 2 && position.y < otherKnot.getPosition().y) {
+                moveUpAndRight();
+            } else if (position.x == otherKnot.getPosition().x - 2 && position.y > otherKnot.getPosition().y) {
+                moveDownAndRight();
+            } else if (position.y == otherKnot.getPosition().y + 2 && position.x == otherKnot.getPosition().x) {
+                moveDown();
+            } else if (position.y == otherKnot.getPosition().y + 2 && position.x < otherKnot.getPosition().x) {
+                moveDownAndRight();
+            } else if (position.y == otherKnot.getPosition().y + 2 && position.x > otherKnot.getPosition().x) {
+                moveDownAndLeft();
+            } else if (position.y == otherKnot.getPosition().y - 2 && position.x == otherKnot.getPosition().x) {
+                moveUp();
+            } else if (position.y == otherKnot.getPosition().y - 2 && position.x < otherKnot.getPosition().x) {
+                moveUpAndRight();
+            } else if (position.y == otherKnot.getPosition().y - 2 && position.x > otherKnot.getPosition().x) {
+                moveUpAndLeft();
+            }
+            visitedPositions.add(position);
+        }
+
+        private boolean isTail() {
+            return null == followingKnot;
+        }
+
+        private void moveFollowingKnot() {
+            if (!isTail() && followingKnot.shouldMove()) {
+                followingKnot.moveTowards(this);
+            }
+        }
+
+        public void moveUp() {
+            position = new Position(position.x, position.y + 1);
+            moveFollowingKnot();
+        }
+
+        public void moveRight() {
+            position = new Position(position.x + 1, position.y);
+            moveFollowingKnot();
+        }
+
+        public void moveDown() {
+            position = new Position(position.x, position.y - 1);
+            moveFollowingKnot();
+        }
+
+        public void moveLeft() {
+            position = new Position(position.x - 1, position.y);
+            moveFollowingKnot();
+        }
+
+        public void moveUpAndLeft() {
+            position = new Position(position.x - 1, position.y + 1);
+            moveFollowingKnot();
+        }
+
+        public void moveUpAndRight() {
+            position = new Position(position.x + 1, position.y + 1);
+            moveFollowingKnot();
+        }
+
+        public void moveDownAndLeft() {
+            position = new Position(position.x - 1, position.y - 1);
+            moveFollowingKnot();
+        }
+
+        public void moveDownAndRight() {
+            position = new Position(position.x + 1, position.y - 1);
+            moveFollowingKnot();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Knot knot = (Knot) o;
+            return Objects.equals(position, knot.getPosition());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(position);
+        }
+
+        public Position getPosition() {
+            return position;
+        }
+    }
+}
+
